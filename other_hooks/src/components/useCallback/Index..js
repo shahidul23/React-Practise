9c1750554ad0a2cf5ab28795132d8f1bcbb905a3
Shahidul Islam <96639022+shahidul23@users.noreply.github.com>
2024-01-25 20:22:39 +0600

    callBack, useMemo

--- other_hooks/src/components/useCallback/Index..js
+++ other_hooks/src/components/useCallback/Index..js
@@ -1,13 +1,20 @@
-import React,{useState} from 'react'
+import React,{useCallback, useState} from 'react'
 import Message from './Message'
 
 const Index = () => {
-  const[count, setCount] = useState(0) 
+  const[count, setCount] = useState(0);
+  const[toggle, setToggle] = useState(false); 
+  const handleIncrement = useCallback(() =>{
+    setCount(count + 1)
+  },[count])
+  console.log("Index")
   return (
     <div>
+      {toggle ? "On" : "Off"}
+      <button onClick={() =>setToggle(!toggle)}> Toggle</button>
       <h1>Count : {count}</h1><br/>
       <button onClick={() =>{setCount((count) => count + 1)}}>Incerement</button><br/>
-      <Message numberOfMennage = {count} />
+      <Message numberOfMessage = {count} OnhandleIncrement= {handleIncrement}/>
     </div>
   )
 }
--- other_hooks/src/components/useCallback/Message.js
+++ other_hooks/src/components/useCallback/Message.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { memo, useMemo } from 'react'
 
-const Message = ({numberOfMennage}) => {
+const Message = ({numberOfMessage,OnhandleIncrement}) => {
+
+  const calculatedNumber = useMemo(() =>{
+    let number = 0;
+    for(let index = 0; index<500000000; index ++){
+      number++;
+    }
+    return number;
+  },[])
+  
   return (
     <div>
-    <h4>send {numberOfMennage} messagese</h4>
+    <h3>Number : {calculatedNumber}</h3>  
+    <h4>send {numberOfMessage} messages</h4>
+    <button onClick={OnhandleIncrement}>increment message number</button>
     </div>
   )
 }
 
-export default Message
+export default memo(Message)
